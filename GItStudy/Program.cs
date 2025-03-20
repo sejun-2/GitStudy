9c4a7cfc2e280dc35aca51c765cf4be90d9722cb
sejun <sejun2.lee@gmail.com>
2025-03-20 12:43:48 +0900

    Feat Add Name

--- GItStudy/Program.cs
+++ GItStudy/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Console.Write("이름을 입력해 주세요 : ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine("입력하신 이름은 {0} 입니다.", input);
         }
     }
 }
