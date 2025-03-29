01c2db9498940848e01d0c55064d323462b69741
sejun <sejun2.lee@gmail.com>
2025-03-29 23:21:07 +0900

    arrange

--- 22.4IfState/Program.cs
+++ 22.4IfState/Program.cs
@@ -12,7 +12,7 @@
             int bigger = left > right  ? left : right;  // left가 right 보다 크다면 left출력 아니면 right 출력
             Console.WriteLine(bigger);
 
-            int abs = left >=0? left : right;           // left가 0과 크거나 같다면 left출력 아니면 right 출력
+            int abs = left >= 0 ? left : right;           // left가 0과 크거나 같다면 left출력 아니면 right 출력
             Console.WriteLine(abs);
 
            
