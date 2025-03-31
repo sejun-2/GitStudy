ba140e402e6f6c874f7e67fa91323fe5eaee7264
sejun <sejun2.lee@gmail.com>
2025-03-31 23:46:59 +0900

    graph Practice

--- 0331.Practice2/Program.cs
+++ 0331.Practice2/Program.cs
@@ -4,38 +4,41 @@ namespace _0331.Practice2
 {
     internal class Program
     {
-       
-
+        
         static void Main(string[] args)
         {
-            int size = 10;
-            const int INF = int.MaxValue;
-            // 예시 - 단방향 가중치 그래프 (단절은 최대값으로 표현) INF
-            int[,] matrixGraph = new int[5, 5]
+            // 다음의 조건에 부합하는 가중치 그래프를 구현하시오.
+            int[,] graph0 = new int[8, 8];
+
+            // 노드와 노드를 잇는 간선 생성 시, 간선은 반드시 가중치를 지녀야 한다
+            // 가중치는 음수를 제외한 실수여야 한다.
+            graph0[0, 1] = 10;
+            graph0[0, 2] = 11;
+            graph0[0, 4] = 12;
+            graph0[1, 5] = 13;
+            graph0[1, 6] = 14;
+            graph0[2, 4] = 15;
+            graph0[3, 1] = 16;
+            graph0[3, 5] = 17;
+            graph0[4, 2] = 18;
+            graph0[5, 3] = 19;
+            graph0[5, 6] = 20;
+            graph0[6, 2] = 21;
+            graph0[6, 5] = 22;
+            graph0[6, 7] = 23;
+
+            for (int i=0; i< graph0.GetLength(0); i++)
             {
-            {   0, 132, INF, INF,  16 },
-            {  12,   0, INF, INF, INF },
-            { INF,  38,   0, INF, INF },
-            { INF,  12, INF,   0,  54 },
-            { INF, INF, INF, INF,   0 },
-            };
-
-            // [0,2] => 출발정점(0) -> 도착정점(2)
-            int distance = matrixGraph[0, 2];   // 0에서 2까지 가중치
-            matrixGraph[0, 2] = 10;           // 가중치 변경. 
-
-
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write($"{matrixGraph} 노드 : ");
-                for (int j = 0; j < size; j++)
+                for (int j=0; j< graph0.GetLength(1); j++)
                 {
-                    if (matrixGraph[i, j] != INF && i != j)
+                    if (graph0[i,j] != 0)
                     {
-                        Console.Write($"- {j} 노드, 가중치 {matrixGraph[i, j]} ");
+                        //그래프의 내용 출력 시 각 노드는 다음와 같은 내용으로 인접한 노드들을 출력해야 한다
+                        Console.WriteLine("graph{0}노드 : - graph{1}노드, 가중치 {2}", i,j,graph0[i, j]);
                     }
+                    
                 }
-                Console.WriteLine();
+                
             }
 
         }
